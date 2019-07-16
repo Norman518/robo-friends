4ca68717756c0d605a4e48fc085e0b0820d71edd
Vincent Teas <vincentteas@gmail.com>
2019-07-16 15:18:28 -0700

    Updates

---
+++ precache-manifest.0424a22c04a3b7db058a4f025b51bded.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1112ad15db0a3bef9094854b229f57f7",
+    "url": "/robo-friends-react/index.html"
+  },
+  {
+    "revision": "165c83f85a4584eb65d7",
+    "url": "/robo-friends-react/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "b4d3b224e63fa45eba90",
+    "url": "/robo-friends-react/static/css/main.0a285c52.chunk.css"
+  },
+  {
+    "revision": "165c83f85a4584eb65d7",
+    "url": "/robo-friends-react/static/js/2.590933e2.chunk.js"
+  },
+  {
+    "revision": "b4d3b224e63fa45eba90",
+    "url": "/robo-friends-react/static/js/main.b6c70903.chunk.js"
+  },
+  {
+    "revision": "5101e40ae37239e64fb0",
+    "url": "/robo-friends-react/static/js/runtime~main.fb1d5cdb.js"
+  },
+  {
+    "revision": "82e09f7862a1e26fb7dfe1d0f8adc87f",
+    "url": "/robo-friends-react/static/media/SEGA.82e09f78.TTF"
+  }
+]);
