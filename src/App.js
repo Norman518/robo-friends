5c679cd54ff129bce81bd0cd872f39b0a9b3688d
Vincent Teas <vincentteas@gmail.com>
2019-07-16 13:28:42 -0700

    added a scroll bar

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import CardList from "./CardList";
 import SearchBox from "./SearchBox";
+import Scroll from './Scroll';
 import "./App.css";
 class App extends Component {
   constructor() {
@@ -22,13 +23,18 @@ class App extends Component {
         .toLowerCase()
         .includes(this.state.searchfield.toLowerCase());
     });
-    return (
-      <div className="tc">
-        <h1 className="f1">RoboFriends</h1>
-        <SearchBox searchChange={this.onSearchChange} />
-        <CardList robots={filteredRobots} />
-      </div>
-    );
+    if (this.state.robots.length === 0) {
+      return <h1>Loading...</h1>;
+    } else {
+      return (
+        <div className="tc">
+          <h1 className="f1">RoboFriends</h1>
+          <SearchBox searchChange={this.onSearchChange} />
+         <Scroll>
+          <CardList robots={filteredRobots} /> </Scroll>
+        </div>
+      );
+    }
   }
 }
 
